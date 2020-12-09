e2858b0d5d4c51db1518b70ebb8c8f02e269e374
Bogdan <bogdybv25@gmail.com>
2020-12-10 00:48:26 +0200

    Added support for EnterpriseLibrary.Validator and tested it in main function.

--- LibraryManagement/DomainModel/Author.cs
+++ LibraryManagement/DomainModel/Author.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Practices.EnterpriseLibrary.Validation.Validators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,22 @@ namespace LibraryManagement.DomainModel
     public class Author
     {
         public int Id { get; set; }
+
+        [NotNullValidator(MessageTemplate = "The FirstName cannot be null", Ruleset = "AuthorNotNull")]
+        [StringLengthValidator(2, RangeBoundaryType.Inclusive, 40, RangeBoundaryType.Inclusive, ErrorMessage = "The FirstName should have between {3} and {5} letters", Ruleset = "AuthorNameStringLength")]
+        [RegexValidator(@"^[a-zA-Z]+$", MessageTemplate = "Only characters", Ruleset = "AuthorNameRegex")]
+        [RegexValidator(@"^[A-Z]", MessageTemplate = "Start with capital letter", Ruleset = "AuthorNameRegex")]
         public string FirstName { get; set; }
+
+        [NotNullValidator(MessageTemplate = "The SecondName cannot be null", Ruleset = "AuthorNotNull")]
+        [StringLengthValidator(2, RangeBoundaryType.Inclusive, 40, RangeBoundaryType.Inclusive, ErrorMessage = "The LastName should have between {3} and {5} letters", Ruleset = "AuthorNameStringLength")]
+        [RegexValidator(@"^[a-zA-Z]+$", MessageTemplate = "Only characters", Ruleset = "AuthorNameRegex")]
+        [RegexValidator(@"^[A-Z]", MessageTemplate = "Start with capital letter", Ruleset = "AuthorNameRegex")]
         public string LastName { get; set; }
         public string Adress { get; set; }
+
+        [NotNullValidator(MessageTemplate = "The Gender cannot be null", Ruleset = "AuthorNotNull")]
+        [DomainValidator("m", "f", "M", "F", MessageTemplate = "Unknown gender", Ruleset ="AuthorInvalidGender")]
         public string Gender { get; set; }
         public ICollection<Book> Books { get; set; }
     }
--- LibraryManagement/Program.cs
+++ LibraryManagement/Program.cs
@@ -1,6 +1,7 @@
 ﻿using LibraryManagement.DataMapper;
 using LibraryManagement.DomainModel;
 using LibraryManagement.Utils;
+using Microsoft.Practices.EnterpriseLibrary.Validation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,19 +13,52 @@ namespace LibraryManagement
 {
     class Program
     {
+
+        static void LogResults(ValidationResults results)
+        {
+            foreach (var result in results)
+            {
+                //TO DO : basically... log!
+                Console.WriteLine("Validation error: " + result.Message);
+            }
+        }
+
+        static ValidationResults ValidateEntity<T>(string ruleSet, T entity)
+        {
+            var validator = ValidationFactory.CreateValidator<T>(ruleSet);
+            var results = new ValidationResults();
+            validator.Validate(entity, results);
+
+            return results;
+        }
+
         static void Main(string[] args)
         {
             var author = new Author
             {
                 FirstName = "Bogdan",
-                LastName = "Hanganu"
+                LastName = "Hanganusad",
+                Gender = "F"
+
             };
 
-            var repo = new AuthorRepository(new LibraryDbContext());
+            var ruleSets = new List<string> { "AuthorNotNull", "AuthorNameStringLength", "AuthorNameRegex", "AuthorInvalidGender" };
 
-            var logger = new Logger();
-            logger.LogError("Hello logging world!", MethodBase.GetCurrentMethod());
+            foreach (var ruleSet in ruleSets)
+            {
+                var results = ValidateEntity(ruleSet, author);
+                if (!results.IsValid)
+                {
+                    LogResults(results);
+                    break;
+                }
+            }
 
+
+            // Entity Framework 6 TEST
+            // var repo = new AuthorRepository(new LibraryDbContext());
+            // var logger = new Logger();
+            // logger.LogError("Hello logging world!", MethodBase.GetCurrentMethod());
             // repo.AddAuthor(author);
         }
     }
